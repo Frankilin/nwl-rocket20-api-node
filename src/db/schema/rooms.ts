3cab9cd1b384bd7d44e8722e6c3edf1a77802bb2
Frankilin <frank_teles@hotmail.com>
2025-07-08 16:24:02 -0300

    Feat: Conexao com drizzle

---
+++ src/db/schema/rooms.ts
@@ -0,0 +1,8 @@
+import { uuid, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+
+export const rooms = pgTable("rooms", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  description: text(),
+  createAt: timestamp().defaultNow().notNull(),
+});
