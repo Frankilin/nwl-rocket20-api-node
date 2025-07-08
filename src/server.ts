fd6021c7206c869a0a4dee0320c67a25b304e471
Frankilin <frank_teles@hotmail.com>
2025-07-08 20:09:42 -0300

    Feat: GET room

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
-import { sql } from './db/connections.ts'
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -20,5 +20,5 @@ app.setValidatorCompiler(validatorCompiler);
 app.get("/health", () => {
   return "OK";
 });
-
-app.listen({ port: env.PORT })
+app.register(getRoomsRoute);
+app.listen({ port: env.PORT });
